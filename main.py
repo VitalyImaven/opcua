710fbc6f5b00168d870fd87ceb50404b45526b4b
vitaly grosman <vitaly.grosman@hp.com>
2026-05-04 12:21:10 +0300

    Remove update_varmon.py script for regenerating the VarMonitor variable registry

--- main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-from src.opc_recorder import OPCUARecorder
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    window = OPCUARecorder()
-    window.show()
-    sys.exit(app.exec_()) 
